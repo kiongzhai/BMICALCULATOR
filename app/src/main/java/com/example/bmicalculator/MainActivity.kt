2e505df03230688faa1e317c16707596d7b2a2d5
anreh <anurehka@gmail.com>
2019-11-11 13:19:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+            calculateBMI()
+        }
+
+        buttonReset.setOnClickListener{
+            reset()
+        }
+    }
+
+    private fun calculateBMI(){
+        if(editTextWeight.text.toString().isEmpty()){
+            editTextWeight.setError(getString(R.string.input_error))
+            return
+        }
+        if(editTextHeight.text.toString().isEmpty()){
+            editTextHeight.setError(getString(R.string.input_error))
+            return
+        }
+
+        val weight =editTextWeight.text.toString().toFloat()
+        val height =editTextHeight.text.toString().toFloat()
+        val bmi = weight / (height/100).pow(2)
+
+        if(bmi <18.5){
+            textViewBMI.text = String.format("%s %.2f (%s)",
+                getString(R.string.bmi), bmi, getString(R.string.under))
+            imageViewProfilePicture.setImageResource(R.drawable.under)
+        }
+
+        if(bmi >18.5&&bmi<24.5){
+            textViewBMI.text = String.format("%s %.2f (%s)",
+                getString(R.string.bmi), bmi, getString(R.string.normal))
+            imageViewProfilePicture.setImageResource(R.drawable.normal)
+        }
+
+        if(bmi >24.5){
+            textViewBMI.text = String.format("%s %.2f (%s)",
+                getString(R.string.bmi), bmi, getString(R.string.over))
+            imageViewProfilePicture.setImageResource(R.drawable.over)
+        }
+
+    }
+
+    private fun reset(){
+        editTextWeight.setText(" ")
+        editTextHeight.setText(" ")
+    }
+
+}
